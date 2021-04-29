5249b594d7362b7babfd50d8cc2a9dd5ee8b15ea
shinychan95 <shinychan95@gmail.com>
2021-04-29 16:57:50 +0900

    Add the client and server code

--- difficulty-one/client.c
+++ difficulty-one/client.c
@@ -2,60 +2,169 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define BUF_SIZE 30
 void error_handling(char *message);
+void print_select_message(void);
 
 int main(int argc, char *argv[])
 {
 	int sock;
+    int select;
+    int str_len;
+
+    char fname[100];
 	FILE *fp;
 	
 	char buf[BUF_SIZE];
-	int read_cnt, recv_size, file_size;
-	struct sockaddr_in serv_adr, from_adr;
-	if (argc != 3) {
-		printf("Usage: %s <IP> <port>\n", argv[0]);
-		exit(1);
-	}
-	
-	fp = fopen("ufile_server.c", "wb");
-	sock = socket(PF_INET, SOCK_DGRAM, 0);   
+    char out[BUF_SIZE];
+    char message[BUF_SIZE];
 
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_adr.sin_port = htons(atoi(argv[2]));
+	int read_cnt, file_size;
+	struct sockaddr_in serv_adr;
 
-	connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
-	
-	// Send start message
-	strcpy(buf, "Let's start!");
-	write(sock, buf, strlen(buf));
-	printf("%s\n", buf);
+    clock_t start, end;
+	float rtt;
+    
+    while (1) {
+        print_select_message();
 
-	// Receive file size
-	read(sock, buf, 4);
-	file_size = atoi(buf);
-	printf("File size=%d bytes\n", file_size);
+        printf(":");
+        scanf("%d", &select);
+        while (getchar() != '\n');
 
-	/// 
-	memset(buf, 0, BUF_SIZE);
-	recv_size = 0;
-	while (recv_size < file_size)
-	{
-		read_cnt = read(sock, buf, BUF_SIZE);
-		fwrite((void*)buf, 1, read_cnt, fp);
-		recv_size += read_cnt;
-	}
-	
-	puts("Received file data");
-	write(sock, "Thank you", 10);
-	fclose(fp);
-	close(sock);
-	return 0;
+        switch (select) {   
+            case 1: // File Transfer
+                printf("Input the file name\n");
+                printf(":");
+                fgets(fname, 100, stdin);
+                printf("\n");   
+                fname[strlen(fname) - 1] = '\0';
+                
+                fp = fopen(fname, "rb");
+                if (fp == NULL) break;
+
+                sock = socket(PF_INET, SOCK_DGRAM, 0);   
+                
+                memset(&serv_adr, 0, sizeof(serv_adr));
+                serv_adr.sin_family = PF_INET;
+                serv_adr.sin_addr.s_addr = inet_addr("127.0.0.1");
+                serv_adr.sin_port = htons(8080);
+
+                connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+
+                // Send the request type
+                write(sock, "f", 1);
+
+                // Send file size to server 
+                fseek(fp, 0, SEEK_END);
+                file_size = ftell(fp);
+                fseek(fp, 0, SEEK_SET);
+                sprintf(buf, "%d", file_size);
+                write(sock, buf, BUF_SIZE);
+
+
+                // Send file data to server
+                int count = 1;
+                while(1)
+                {   
+                    read_cnt = fread((void*)buf, 1, BUF_SIZE, fp);
+                    strcpy(out, buf);
+                    char *ptr = strtok(out, "\n");
+                    printf("[SENDING %d/%d] Data: %s...\n", count++, file_size / BUF_SIZE + 1, ptr);
+                    if (read_cnt < BUF_SIZE)
+                    {
+                        write(sock, buf, read_cnt);
+                        break;
+                    }
+                    write(sock, buf, read_cnt);
+                    
+                }
+                printf("[SUCCESS] Data transfer complete.\n");
+                
+                read(sock, buf, BUF_SIZE);
+
+                fclose(fp);
+                close(sock);
+
+                break;
+
+            case 2: // Send Message
+                sock = socket(PF_INET, SOCK_DGRAM, 0);   
+                if (sock == -1)
+                    error_handling("socket() error");
+                
+                memset(&serv_adr, 0, sizeof(serv_adr));
+                serv_adr.sin_family = PF_INET;
+                serv_adr.sin_addr.s_addr = inet_addr("127.0.0.1");
+                serv_adr.sin_port = htons(8080);
+                
+                connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+
+                
+
+                while (1)
+                {
+                    printf("Type the message for server (STOP to quit)\n");
+                    printf(":");
+                    fgets(message, sizeof(message), stdin);
+                    message[strlen(message) - 1] = 0;
+                    printf("\n");    
+                    if (!strcmp(message,"STOP") || !strcmp(message,"stop"))	
+                        break;
+                    
+                    // Send the request type
+                    write(sock, "m", 1);
+
+                    start = clock();
+                    write(sock, message, strlen(message));
+                    str_len = read(sock, message, sizeof(message)-1);
+                    end = clock();
+                    message[str_len] = 0;
+
+                    if (strcmp(message, "close") == 0) {
+                        start = clock();
+                        float count = 1;
+                        printf("Count :: 1\n");
+                        while(1) {
+                            end = clock();
+                            if ((float)(end - start)/CLOCKS_PER_SEC > count) {
+                                count++;
+                                if (count == 6) {
+                                    break;
+                                }
+                                printf("Count :: %d\n", (int)count);
+                            }
+                        }
+
+                        printf("Time Out\n");
+                        return 0;
+                    }
+                    
+                    rtt = (float)(end - start)/CLOCKS_PER_SEC;
+                    
+                    printf("[SERVER RECEIVED]: %s\n", message);
+                    printf("[RTT of Message] %f\n", rtt);
+                }	
+
+                close(sock);
+
+                break;
+
+            case 3:
+                return 0;
+
+            default:
+                printf("\nWrong. Again.\n");
+
+        }
+    }
+    
+    return 0;
 }
 
 void error_handling(char *message)
@@ -63,4 +172,13 @@ void error_handling(char *message)
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
+}
+
+void print_select_message(void)
+{
+	printf("---------------------------\n");
+    printf("1. File Transfer\n");
+    printf("2. Send Message\n");
+    printf("3. Finish\n");
+    printf("\n");
 }
--- difficulty-one/server.c
+++ difficulty-one/server.c
@@ -10,65 +10,88 @@ void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	int serv_sock;
-	FILE * fp;
+	int sock;
+    int seqnum = 1;
+	FILE *fp;
+	
 	char buf[BUF_SIZE];
-	int read_cnt, file_size;
-	
+    char out[BUF_SIZE];
+    char message[BUF_SIZE];
+	int read_cnt, recv_size, file_size, str_len;
 	struct sockaddr_in serv_adr, clnt_adr;
-	socklen_t clnt_adr_sz;
+    socklen_t clnt_adr_sz;
 	
-	if (argc != 2) {
-		printf("Usage: %s <port>\n", argv[0]);
-		exit(1);
-	}
-	
-	fp = fopen("ufile_server.c", "rb"); 
-	serv_sock = socket(PF_INET, SOCK_DGRAM, 0);   
-	
+	sock = socket(PF_INET, SOCK_DGRAM, 0);   
+
 	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr.sin_port = htons(atoi(argv[1]));
+	serv_adr.sin_family = PF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_adr.sin_port = htons(8080);
 	
-	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+	if (bind(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
 		error_handling("bind() error");
 	
-	// Receive request meesage from client 
-	clnt_adr_sz = sizeof(clnt_adr);
-	recvfrom(serv_sock, buf, BUF_SIZE, 0, 
-				(struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-	printf("Client's message: %s\n", buf);
+    printf("[STARTING] UDP File Server is starting...\n");
 
-	// Send file size to client 
-	fseek(fp, 0, SEEK_END);
-	file_size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	sprintf(buf, "%d", file_size);
-	sendto(serv_sock, buf, BUF_SIZE, 0, 
-				(struct sockaddr*)&clnt_adr, clnt_adr_sz);
+    while (1) {
+        // Receive request meesage from client 
+        clnt_adr_sz = sizeof(clnt_adr);
+        recvfrom(sock, buf, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
 
-	// Send file data to client 
-	while(1)
-	{
-		read_cnt = fread((void*)buf, 1, BUF_SIZE, fp);
-		if (read_cnt < BUF_SIZE)
-		{
-			sendto(serv_sock, buf, read_cnt, 0, 
-					(struct sockaddr*)&clnt_adr, clnt_adr_sz);
-			break;
-		}
-		sendto(serv_sock, buf, read_cnt, 0, 
-				(struct sockaddr*)&clnt_adr, clnt_adr_sz);
-		printf("Send data to client\n");
-	}
-	
-	recvfrom(serv_sock, buf, BUF_SIZE, 0, 
-				(struct sockaddr*)&clnt_adr, &clnt_adr_sz); 
-	printf("Message from client: %s \n", buf);
+        if (buf[0] == 'f') {
+            printf("File Transfer Request\n");
 
-	fclose(fp);
-	close(serv_sock);
+            fp = fopen("server.txt", "wb");
+
+            recvfrom(sock, buf, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+            file_size = atoi(buf);
+            printf("File size=%d bytes\n", file_size);
+
+            memset(buf, 0, BUF_SIZE);
+            recv_size = 0;
+            int count = 1;
+            while (recv_size < file_size)
+            {
+                read_cnt = recvfrom(sock, buf, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+                strcpy(out, buf);
+                char *ptr = strtok(out, "\n");
+                printf("[RECEIVING %d/%d] Data: %s\n", count++, file_size / BUF_SIZE + 1, ptr);
+                fwrite((void*)buf, 1, read_cnt, fp);
+                recv_size += read_cnt;
+            }
+            printf("[SUCCESS] Data transfer complete.\n");
+            sendto(sock, "finished", 8, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+            fclose(fp);
+        }
+        else if (buf[0] == 'm') {
+            clnt_adr_sz = sizeof(clnt_adr);
+            str_len = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+            message[str_len - 1] = 0;
+            if (seqnum == 11 || strcmp(message, "Start Alarm") == 0) {
+                sendto(sock, "close", 5, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+                printf("[ClOSING]: Close the server\n");
+                break;
+            }
+            
+            printf("[RECEIVING] Message: %s\n", message);
+            
+            for (int i = 0; i < (int)strlen(message); i++){
+                if (message[i] >= 'a' && message[i] <= 'z'){
+                    message[i] -= 32;
+                }
+            }
+
+            sendto(sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+            printf("[SENDING] Message: %s\n", message);
+            printf("[Sequence NUMBER]: %d\n", seqnum);
+            seqnum++;
+        }
+        else {
+            printf("Irrecognizable Request Comes\n");
+        }        
+    }
+
+    close(sock);
 	return 0;
 }
 
